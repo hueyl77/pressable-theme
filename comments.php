5007f5b96f71f337eb9386d08218fbe125b95e39
Thomas Griffin <thomas@thomasgriffinmedia.com>
2013-11-12 10:19:17 -0500

    Address #2 to add in comments and styling for them.

--- comments.php
+++ comments.php
@@ -25,7 +25,7 @@ if ( post_password_required() )
 		</h2>
 
 		<ol class="commentlist">
-			<?php wp_list_comments( array( 'callback' => 'twentytwelve_comment', 'style' => 'ol' ) ); ?>
+			<?php wp_list_comments( array( 'callback' => 'pressable_comments', 'style' => 'ol' ) ); ?>
 		</ol><!-- .commentlist -->
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
